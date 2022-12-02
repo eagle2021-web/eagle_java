b3718d05c9e1294b7ab22052281428b8567d8cbb
eagle <chenwangying16@163.com>
2022-12-02 23:58:28 +0800

    eagle_leetcode

---
+++ eagle_leetcode/src/main/java/com/eagle/Main.java
@@ -0,0 +1,23 @@
+package com.eagle;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        Solution s = new Solution();
+
+    }
+    public int reversePairs(int[] nums) {
+
+    }
+
+}
