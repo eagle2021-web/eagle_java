e56ceb44391fbc07907a61a6a00889459288fcc0
eagle <chenwangying16@163.com>
2022-12-03 23:52:00 +0800

    221203

--- eagle_leetcode/src/main/java/com/eagle/Main.java
+++ eagle_leetcode/src/main/java/com/eagle/Main.java
@@ -1,23 +1,32 @@
 package com.eagle;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import com.eagle.common.TreeNode;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
-
+@SuppressWarnings("all")
 class Solution {
+    int res = Integer.MIN_VALUE;
     public static void main(String[] args) {
-        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
-        Solution s = new Solution();
-
-    }
-    public int reversePairs(int[] nums) {
 
     }
 
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node==null)return 0;
+        int l = dfs(node.left), r = dfs(node.right);
+        l = Math.max(l, 0);
+        r = Math.max(r, 0);
+        int max = node.val + l + r;
+        res = Math.max(max, res);
+        return node.val + Math.max(l, r);
+    }
 }
